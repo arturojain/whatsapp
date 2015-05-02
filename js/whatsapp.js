ecf9953e01d326e05c9fb6d02b98e0b1a8441ab7
arturojain <arturojain@gmail.com>
2015-05-02 08:06:54 -0500

    v1.0

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,4 +1,18 @@
 (function(){
+    var $startConversation = $('#start-conversation');
+    var $startConversationLink = $('#start-conversation-link');
+    var $encrypt = $('#encrypt');
+    var $encryptLink = $('#encrypt-link');
+    var $decrypt = $('#decrypt');
+    var $inputText = $('#input-text');
+    var $outputText = $('#output-text');
+
+    $encrypt.click(function(){
+        $encryptLink.attr('href', encrypt($inputText.val()));
+    });
+    //$decrypt.click(function(){
+        //$outputText.val(decrypt(getParameterByName('msg')));
+    //});
     /*
      * Workflow:
      * 1. New conversation (button, creates handshake sending a link to the other user, only the other user can send a new message) 
@@ -10,20 +24,20 @@
      * URL should be ?msg=X&p=X&g=X&A=X&B=X
      */
     var handshake = function(){
-        var variables = setVariables(getParameterByName('p'),getParameterByName('g'),document.cookie,getParameterByName('A'),getParameterByName('B'));
-        document.cookie = variables.a;
-        return "whatsapp://send?text=" + encodeURIComponent("http://iomediamx.com/cryptowhats?p=" + variables.p + "&g=" + variables.g +
-                "&A=" + variables.B + "&B=" + variables.A);
+        var variables = initializeVariables(readCookie("a"));
+        createCookie("a", variables.a, 7);
+        return "whatsapp://send?text=" + encodeURIComponent("http://iomediamx.com/secure?p=" + variables.p + "&g=" + variables.g
+                + "&B=" + variables.A);
     }
     var decrypt = function(text){
-        var variables = setVariables(getParameterByName('p'),getParameterByName('g'),document.cookie,getParameterByName('A'),getParameterByName('B'));
+        var variables = setVariables(getParameterByName('p'),getParameterByName('g'),readCookie("a"),getParameterByName('A'),getParameterByName('B'));
         return CryptoJS.AES.decrypt(text,variables.s).toString(CryptoJS.enc.Utf8);
     };
     var encrypt = function(text){
-        var variables = setVariables(getParameterByName('p'),getParameterByName('g'),document.cookie,getParameterByName('A'),getParameterByName('B'));
-        document.cookie = variables.a;
-        var encrypted = CryptoJS.AES.encrypt(text,variables.s);
-        return "whatsapp://send?text=" + encodeURIComponent("http://iomediamx.com/cryptowhats?msg=" + encrypted.toString() + "&p=" + variables.p + "&g=" + variables.g +
+        var variables = setVariables(getParameterByName('p'),getParameterByName('g'),readCookie("a"),getParameterByName('A'),getParameterByName('B'));
+        createCookie("a", variables.a, 7);
+        var encrypted = CryptoJS.AES.encrypt(text,""+variables.s);
+        return "whatsapp://send?text=" + encodeURIComponent("http://iomediamx.com/secure?msg=" + encrypted.toString() + "&p=" + variables.p + "&g=" + variables.g +
                 "&A=" + variables.B + "&B=" + variables.A);
     };
     /*
@@ -32,21 +46,43 @@
      * A is our user public variable
      * B is the other user public variable
      */
-    var setVariables(p,g,a,A,B){
-        if (typeof p === 'undefined') {p = BigInteger(newPrime())}
-        if (typeof g === 'undefined') {g = BigInteger(newPrime())}
-        //creates new private a if not defined
-        if (typeof a === 'undefined') {a = BigInteger(Math.floor((Math.random()*1000)+100))}
-        if (typeof A === 'undefined') {A = BigInteger(Math.pow(g, a)%p)}
-        var s = Math.pow(B,a)%p;
+    var initializeVariables = function(a){
+        var p = BigInteger(newPrime());
+        var g = BigInteger(newPrime());
+        if (typeof a === 'undefined' || a === null) {a = BigInteger(Math.floor((Math.random()*100)+10))}
+        //if (A == "") {A = BigInteger(Math.pow(g, a)%p)}
+        var A = g.pow(a);
+        A = A.remainder(p);
         return variables = {
-            p: p,
-            g: g,
-            a: a,
-            A: A,
-            B: B,
-            s: s
-        }:
+            p: p.toString(),
+            g: g.toString(),
+            a: a.toString(),
+            A: A.toString()
+        };
+    };
+    var setVariables = function(p,g,a,A,B){
+        if (p == "") {p = BigInteger(newPrime())}else{p = BigInteger(p)};
+        if (g == "") {g = BigInteger(newPrime())}else{g = BigInteger(g)};
+        //creates new private a if not defined
+        if (typeof a === 'undefined' || a === null) {a = BigInteger(Math.floor((Math.random()*100)+10))}else{a = BigInteger(a)};
+        //if (A == "") {A = BigInteger(Math.pow(g, a)%p)}
+        if (A == "") {
+            A = g.pow(a);
+            A = A.remainder(p);
+        }else{
+            A = BigInteger(A)
+        };
+        var B = BigInteger(B);
+        var s = B.pow(a);
+        s = s.remainder(p);
+        return variables = {
+            p: p.toString(),
+            g: g.toString(),
+            a: a.toString(),
+            A: A.toString(),
+            B: B.toString(),
+            s: s.toString()
+        };
     };
     /*
      * Helping function
@@ -58,7 +94,7 @@
         return results === null ? "" : decodeURIComponent(results[1]);
     };
     var newPrime = function(){
-        var num = Math.floor((Math.random()*1000)+100);
+        var num = Math.floor((Math.random()*100)+10);
         var res = (isPrime(num));
         if(res){
             return num;
@@ -66,7 +102,7 @@
             return newPrime();
         }
     };
-    var isPrime = function(){
+    var isPrime = function(num){
         if(num==0 || num==1)
             return true;
         if(num==2)
@@ -100,4 +136,36 @@
             }
         return roots;
     };
-A)()
+    var createCookie = function(name,value,days) {
+        if (days) {
+                var date = new Date();
+                date.setTime(date.getTime()+(days*24*60*60*1000));
+                var expires = "; expires="+date.toGMTString();
+            }
+        else var expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+    }
+    var readCookie = function(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+        return null;
+    }
+    var eraseCookie = function(name) {
+        createCookie(name,"",-1);
+    }
+
+    if(getParameterByName('B') == ""){
+        $startConversationLink.attr('href', handshake());
+    } else {
+        $startConversation.hide();
+        if(getParameterByName('msg') != ""){
+            //$inputText.val(getParameterByName('msg'));
+            $inputText.val(decrypt(getParameterByName('msg')));
+        }
+    }
+})();
